1c43892ae68d5436bd518153cf0c8dd5f7c3a127
vagrant <mveri.bruno@gmail.com>
2021-01-13 23:23:20 +0000

    Experimented with hard code, move commands

--- client.js
+++ client.js
@@ -7,6 +7,18 @@ const connect = function() {
   });
 
   conn.setEncoding('utf8'); 
+ 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: BMV');
+    // conn.write('Move: up'); /***Hard-code** */
+    // setTimeout(() => {
+    //   conn.write('Move: right');
+    // }, 50);
+  });
   
   conn.on('data', (data) => {
     console.log('Server says: ', data);
