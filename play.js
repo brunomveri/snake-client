922eff6e0b6fe77e75a3806f7f928d36c0fa8d8f
vagrant <mveri.bruno@gmail.com>
2021-01-13 23:58:52 +0000

    Stdin setup

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
 const { connect } = require('./client');
 
 console.log('Connecting ...');
+
 connect();
+
+const setupInput = function() {
+  
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', (keyboard) => handleUserInput(keyboard));
+
+  return stdin;
+};
+
+function handleUserInput (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+setupInput();
