71463730f03a9456136bf8784d56557ead8eb6cf
vagrant <mveri.bruno@gmail.com>
2021-01-14 01:03:55 +0000

    Sending movement commands

--- input.js
+++ input.js
@@ -1,20 +1,35 @@
-const setupInput = function() {
-  
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  stdin.on('data', (keyboard) => handleUserInput(keyboard));
+  stdin.on('data', handleUserInput);
 
   return stdin;
 };
 
 function handleUserInput (key) {
-  if (key === '\u0003') {
+  if (key === '\u0003' || key === 'x') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up')
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
 }
+
 
 module.exports = {
   setupInput
